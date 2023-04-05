0eef4506dcbe2388887b718aa933a22664c4fd69
unknown <nagajyothim44@gmail.com>
2023-04-05 14:55:00 +0530

    demart initial commit

---
+++ client/src/test/java/demart/client/EntitiesEndpointTest.java
@@ -0,0 +1,18 @@
+package demart.client;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import java.net.URI;
+import com.oneandone.typedrest.CollectionEndpointImpl;
+import demart.model.*;
+import org.junit.Before;
+
+public class EntitiesEndpointTest extends AbstractEndpointTest {
+    private CollectionEndpointImpl<MyEntity> endpoint;
+
+    @Before
+    @Override
+    public void before() {
+        super.before();
+        endpoint = new CollectionEndpointImpl<>(entryEndpoint, "entities", MyEntity.class);
+    }
+}
